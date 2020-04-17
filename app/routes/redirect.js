e2065b8c355bb63cb452780b071464a4161f799e
TianchantgZhang <lesdanita@outlook.com>
2020-04-17 11:30:48 -0400

    Save request data.

--- app/routes/redirect.js
+++ app/routes/redirect.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var urlService = require("../services/urlService");
+var statsService = require("../services/statsService");
 var path = require('path');
 router.get('*', function (req, res){
     var shortUrl = req.originalUrl.slice(1);
     urlService.getLongUrl(shortUrl, function (data) {
         if (data) {
             res.redirect(data.longUrl);
+            statsService.logRequest(shortUrl, req);
         }
         else {
             res.sendFile("404.html", {root: path.join(__dirname, '../public/views/')});
--- app/server.js
+++ app/server.js
@@ -4,10 +4,12 @@ var restRouter = require('./routes/rest');
 var redirectRouter = require('./routes/redirect');
 var indexRouter = require('./routes/index');
 var mongoose = require('mongoose');
+var useragent = require('express-useragent');
 
 mongoose.connect("mongodb+srv://user:user@tinyurl-cwyb9.mongodb.net/test?retryWrites=true&w=majority",{useNewUrlParser: true, useUnifiedTopology: true})
 
 app.use('/public', express.static(__dirname + "/public"));
+app.use(useragent.express());
 app.use("/api/v1", restRouter);
 app.use('/', indexRouter);
 app.use("/:shortUrl", redirectRouter);
